82d099a91241a6e5889b5557e9a21f31f63215b3
cole_c <cole.e.cummings@gmail.com>
2020-01-23 21:24:38 -0600

    Added damage on combat/health on enemy

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,23 +9,50 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
-            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
-            if (!isInRange)
+            if (!IsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.position);
             }
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehavior();
             }
 
+        }
+
+        private void AttackBehavior()
+        {
+            if(timeSinceLastAttack > timeBetweenAttacks)
+            {
+                //This will trigger the Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+        }
+
+        //Animation Event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
+        }
+
+        private bool IsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
         }
 
         public void Attack(CombatTarget target)
