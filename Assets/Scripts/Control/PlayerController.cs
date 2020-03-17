584531b0c05b5117fa63a7bc27802f5542bf33ac
cole_c <cole.e.cummings@gmail.com>
2020-03-17 17:10:59 -0500

    Added patrol speed/chase speed to guards, started to add opening cutscene

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -31,7 +31,7 @@ namespace RPG.Control
             {
                 if (Input.GetMouseButton(0))
                 {
-                    GetComponent<Mover>().MoveTo(hit.point);
+                    GetComponent<Mover>().MoveTo(hit.point, 1f);
                     GetComponent<Fighter>().Cancel(); //Run away from combat
                 }
                 return true;
