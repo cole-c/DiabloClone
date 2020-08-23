992567557ce4920d2dae49ce2d96919a44fdee4f
cole_c <cole.e.cummings@gmail.com>
2020-08-23 13:25:07 -0500

    new branch to figure out nested prefab damage UI bug

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
